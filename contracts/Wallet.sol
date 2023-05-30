75c176b985d0cf3de33aa243cd339b1282bc819b
tauseedzaman <tauseedzamanofsorana@gmail.com>
2023-05-30 17:23:40 +0100

    withdraw added and working on deposit feature

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,4 +1,6 @@
 pragma solidity ^0.8.0;
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Wallet {
     struct Token {
@@ -10,4 +12,36 @@ contract Wallet {
     mapping(bytes32 => Token) public tokenMapping;
 
     mapping(address => mapping(bytes32 => uint256)) public balances;
+
+    function addToken(bytes32 ticker, address tokenAddress) external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) external {
+        require(
+            tokenMapping[ticker].tokenAddress != address(0),
+            "Invalid Token"
+        );
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
+    }
+
+    function withdraw(uint amount, bytes32 ticker) external {
+        // validate
+        require(
+            tokenMapping[ticker].tokenAddress != address(0),
+            "Invalid Token"
+        );
+        require(
+            balances[msg.sender][ticker] >= amount,
+            "Invalid Balance amount"
+        );
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
+    }
 }
